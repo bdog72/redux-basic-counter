18556a1cd2ea6c5481475c550fc2c2fec0525e90
Brian Bycynski <high5bri@hotmail.com>
2017-11-30 10:00:16 -0500

    added a delete option

--- src/containers/Counter/Counter.js
+++ src/containers/Counter/Counter.js
@@ -3,6 +3,7 @@ import { connect } from 'react-redux'
 
 import CounterControl from '../../components/CounterControl/CounterControl'
 import CounterOutput from '../../components/CounterOutput/CounterOutput'
+import * as actionTypes from '../../store/actions'
 
 class Counter extends Component {
     render() {
@@ -25,6 +26,24 @@ class Counter extends Component {
                     label="Subtract 5"
                     clicked={this.props.onSubtractFiveCounter}
                 />
+                <hr />
+                <button
+                    onClick={() => this.props.onStoreResult(this.props.ctr)}
+                >
+                    Store Result
+                </button>
+                <ul>
+                    {this.props.storedResults.map(strResult => (
+                        <li
+                            key={strResult.id}
+                            onClick={() =>
+                                this.props.onDeleteResult(strResult.id)
+                            }
+                        >
+                            {strResult.value}
+                        </li>
+                    ))}
+                </ul>
             </div>
         )
     }
@@ -32,46 +51,23 @@ class Counter extends Component {
 
 const mapStateToProps = state => {
     return {
-        ctr: state.counter
+        ctr: state.ctr.counter,
+        storedResults: state.res.results
     }
 }
 
 const mapDispatchToProps = dispatch => {
     return {
-        onIncrementCounter: () => dispatch({ type: 'INCREMENT' }),
-        onDecrementCounter: () => dispatch({ type: 'DECREMENT' }),
-        onAddFiveCounter: () => dispatch({ type: 'ADD', val: 5 }),
-        onSubtractFiveCounter: () => dispatch({ type: 'SUBTRACT', val: 5 })
+        onIncrementCounter: () => dispatch({ type: actionTypes.INCREMENT }),
+        onDecrementCounter: () => dispatch({ type: actionTypes.DECREMENT }),
+        onAddFiveCounter: () => dispatch({ type: actionTypes.ADD, val: 5 }),
+        onSubtractFiveCounter: () =>
+            dispatch({ type: actionTypes.SUBTRACT, val: 5 }),
+        onStoreResult: result =>
+            dispatch({ type: actionTypes.STORE_RESULT, result: result }),
+        onDeleteResult: id =>
+            dispatch({ type: actionTypes.DELETE_RESULT, resultElId: id })
     }
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(Counter)
-
-// state = {
-//     counter: 0
-// }
-
-// counterChangedHandler = (action, value) => {
-//     switch (action) {
-//         case 'inc':
-//             this.setState(prevState => {
-//                 return { counter: prevState.counter + 1 }
-//             })
-//             break
-//         case 'dec':
-//             this.setState(prevState => {
-//                 return { counter: prevState.counter - 1 }
-//             })
-//             break
-//         case 'add':
-//             this.setState(prevState => {
-//                 return { counter: prevState.counter + value }
-//             })
-//             break
-//         case 'sub':
-//             this.setState(prevState => {
-//                 return { counter: prevState.counter - value }
-//             })
-//             break
-//     }
-// }
